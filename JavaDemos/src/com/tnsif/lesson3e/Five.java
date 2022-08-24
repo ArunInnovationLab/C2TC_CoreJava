eed48e950ca2300a9bfd7431b3a532b7bb0457c9
Arunkumar <arunkumaryargoal@gmail.com>
2022-08-24 17:51:17 +0530

    commit

---
+++ JavaDemos/src/com/tnsif/lesson3e/Five.java
@@ -0,0 +1,14 @@
+package com.tnsif.lesson3e;
+
+class FiveDemo{
+	
+}
+
+public class Five {
+
+	public static void main(String[] args) {
+
+	
+	}
+
+}
---
+++ JavaDemos/src/com/tnsif/lesson3f/Six.java
@@ -0,0 +1,14 @@
+package com.tnsif.lesson3f;
+
+class DemoSix{
+	
+}
+
+public class Six {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
