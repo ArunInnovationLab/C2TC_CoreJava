6449d287cfe471267faae324490cc82159f77d14
Arunkumar <arunkumaryargoal@gmail.com>
2022-08-24 21:35:18 +0530

    commit

--- JavaDemos/src/com/tnsif/lesson3e/Five.java
+++ JavaDemos/src/com/tnsif/lesson3e/Five.java
@@ -1,14 +1,14 @@
 package com.tnsif.lesson3e;
-
-class FiveDemo{
+import com.tnsif.lesson3f.*;
+public class Five{
 	
-}
-
-public class Five {
-
 	public static void main(String[] args) {
-
-	
+		// TODO Auto-generated method stub
+		 
+		Six obj = new Six();
+		obj.dispOutput();	
+		
 	}
 
 }
+
--- JavaDemos/src/com/tnsif/lesson3f/Six.java
+++ JavaDemos/src/com/tnsif/lesson3f/Six.java
@@ -1,14 +1,16 @@
 package com.tnsif.lesson3f;
 
-class DemoSix{
-	
-}
-
 public class Six {
 
+	public void dispOutput() {
+		System.out.println("I am Six");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	
 	}
 
 }
+
