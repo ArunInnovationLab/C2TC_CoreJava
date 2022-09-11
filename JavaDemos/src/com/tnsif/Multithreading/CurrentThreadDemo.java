dc7e42c0b38fe4a15daae97a138a35482664d9cc
Arunkumar <arunkumaryargoal@gmail.com>
2022-09-11 17:26:15 +0530

    commit

--- JavaDemos/src/com/tnsif/Multithreading/CurrentThreadDemo.java
+++ JavaDemos/src/com/tnsif/Multithreading/CurrentThreadDemo.java
@@ -10,14 +10,7 @@ public class CurrentThreadDemo {
 		t.setName("My thread");
 		System.out.println("After name change: "+t);
 		
-//		try {
-//			for(int i=5;i>0;i--) {
-//				System.out.println(i);
-//				Thread.sleep(1000);
-//			}
-//		}catch(InterruptedException e) {
-//			System.out.println("Main thread interrupted");
-//		}
+
 		for(int i=5;i>0;i--) {
 			System.out.println(i);
 			try {
