a014900eb4d4b8515b726f40f85d157040736627
Arunkumar <arunkumaryargoal@gmail.com>
2022-09-18 10:38:33 +0530

    commit

--- JavaDemos/src/com/tnsif/LocalDateAndTime/PeriodAndDurationDemo.java
+++ JavaDemos/src/com/tnsif/LocalDateAndTime/PeriodAndDurationDemo.java
@@ -1,6 +1,8 @@
 package com.tnsif.LocalDateAndTime;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 
@@ -22,10 +24,13 @@ public class PeriodAndDurationDemo {
 	    System.out.println(between);
 	    
 	    
-	    
+	    System.out.println("----------------");
 	    //Duration class is used to deal with Time. It represents the quantity of time in terms of seconds and nano seconds.
+	    LocalTime initialTime = LocalTime.of(06, 30, 0);
+	    LocalTime finalTime = initialTime.plus(Duration.ofSeconds(30));
 	    
-	    
+	    System.out.println(Duration.between(initialTime, finalTime).getSeconds());
+	    System.out.println(ChronoUnit.SECONDS.between(initialTime, finalTime));
 	   
 	   
 	}
