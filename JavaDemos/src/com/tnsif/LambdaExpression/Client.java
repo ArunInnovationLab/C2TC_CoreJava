c6d3227fe587cdf657d8772c1f0d0b6597277fb5
Arunkumar <arunkumaryargoal@gmail.com>
2022-09-26 13:19:14 +0530

    commit

--- JavaDemos/src/com/tnsif/LambdaExpression/Client.java
+++ JavaDemos/src/com/tnsif/LambdaExpression/Client.java
@@ -1,5 +1,12 @@
 package com.tnsif.LambdaExpression;
 
+import java.util.function.Supplier;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+import java.util.function.Predicate;
+
+
 public class Client {
 
 	public static void main(String[] args) {
@@ -10,10 +17,45 @@ public class Client {
 		}else {
 			System.out.println("b is the maximum");
 		}
-		}
-	;	
 		
+	    };	
+	
 	maximum.FindMax(9, 6);
+	
+	Supplier<String> s1 = ()-> "Hello World";
+	System.out.println(s1.get().toString());
+	
+	BooleanSupplier bs = ()->{ return true;};
+    System.out.println(bs.getAsBoolean());
+	
+	
+	Consumer<String> c = (msg) -> {System.out.println(msg);};
+	c.accept("Good morning");
+	
+	IntConsumer i = (intVariable) -> {System.out.println(intVariable);};
+	i.accept(11);
+	
+	//Even number test
+	Predicate<Integer> pd = num1 -> num1%2==0; 
+	//System.out.println(pd.test(111));
+	
+	// even or odd 
+	int arr[] = {2,3,4};
+	int evencount=0;
+	int oddcount=0;
+	for (int j = 0; j < arr.length; j++) {
+		if(pd.test(arr[j])){
+			evencount++;
+		}else {
+			oddcount++;
+		}
 	}
+	System.out.println("Even "+evencount);
+	System.out.println("Odd "+oddcount);
+	
+	
+	}
+	
+	
 
 }
